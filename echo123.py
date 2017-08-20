6c7345bc444863e01bb6752cd9ecd7f7d9d6376c
tomchen1468357 <tomchen1468357@gmail.com>
2017-08-20 19:56:51 +0800

    Update echo123.py

--- echo123.py
+++ echo123.py
@@ -1,6 +1,7 @@
 import requests
 import re
 import random
+import time
 from bs4 import BeautifulSoup
 from flask import Flask, request, abort
 from imgurpython import ImgurClient
@@ -40,7 +41,7 @@ def callback():
 
     return 200
 
-
+'''
 def pattern_mega(text):
     patterns = [
         'mega', 'mg', 'mu', 'ＭＥＧＡ', 'ＭＥ', 'ＭＵ',
@@ -285,12 +286,104 @@ def panx():
         link = data['href']
         content += '{}\n{}\n\n'.format(title, link)
     return content
+'''
+def money_J_U():
+    resp = requests.get(
+        url=url,
+        cookies={'over18': '1'}
+    )
+    if resp.status_code != 200:
+        print('Invalid url:', resp.url)
+        return None
+    else:
+        #        return resp.text
+        return resp.text
+    allcash = []
+    product = []
+    dodo1 = []
+    allshowcashbuy = []
+    alltaxcashbuy = []
+    allshowcashsell = []
+    alltaxcashsell = []
+    tax1 = []
+    page = get_web_page('http://rate.bot.com.tw/xrt?Lang=zh-TW')
+# print(page)
+    butidodo = BeautifulSoup(page, 'html.parser')
+# print(butidodo)
+    dodo_find = butidodo.find_all("td", "rate-content-cash text-right print_hide")
+# print(dodo_find)
+    cash_doler = butidodo.find_all("div", "visible-phone print_hide")
+# print(cash_doler)
+    tax_find = butidodo.find_all("td", "rate-content-sight text-right print_hide")
+#    print(tax_find)
+    for cashs in cash_doler:
+        wow = cashs.text.strip()
+        allcash.append(wow)
 
+    for tax in tax_find:
+     #   print(dodo)
+        tax2 = tax.text.strip()
+        tax1.append(tax2)
+
+
+# print(dodo_find)
+    for dodo in dodo_find:
+     #   print(dodo)
+        do2 = dodo.text.strip()
+        dodo1.append(do2)
+#    print(dodo1)
+#    print(tax1)
+    for aa in range(len(allcash)):
+        bb = aa * 2
+
+        if allcash[aa] == "美金 (USD)" or allcash[aa] == "日圓 (JPY)":
+            showcashbuy = dodo1[bb]
+            taxcashbuy = tax1[bb]
+            allshowcashbuy.append(showcashbuy)
+            alltaxcashbuy.append(taxcashbuy)
+            showcashsell = dodo1[bb + 1]
+            taxcashsell = tax1[bb + 1]
+            allshowcashsell.append(showcashsell)
+            alltaxcashsell.append(taxcashsell)
+    recodetime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    allcash11 = ["美金(USD)", "日圓(JPY)"]
+    USD_NOW = '\n{} {} \n現金本行:買入{} 賣出{} \n即期本行:買入{} 賣出{}'.format(recodetime, allcash11[0], allshowcashbuy[0], allshowcashsell[0], alltaxcashbuy[0], alltaxcashsell[0])
+    JPY_NOW = '\n{} {} \n現金本行:買入{} 賣出{} \n即期本行:買入{} 賣出{}'.format(recodetime, allcash11[1], allshowcashbuy[1], allshowcashsell[1], alltaxcashbuy[1], alltaxcashsell[1])
+    JPYANDUSD = '{}\n{}'.format(USD_NOW, JPY_NOW)
+    return JPYANDUSD
+
+
+#print(money_J_U())
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     print("event.reply_token:", event.reply_token)
     print("event.message.text:", event.message.text)
+    if event.message.text == "台銀即時匯率":
+        content = money_J_U()
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=content))
+        return 0
+    if event.message.text == "Lativ特價查詢":
+        content = "很抱歉本項服務尚未建置完成，敬請選擇其他服務"
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=content))
+        return 0
+    if event.message.text == "My Protein打幾折":
+        content = "很抱歉本項服務尚未建置完成，敬請選擇其他服務"
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=content))
+        return 0
+    if event.message.text == "奇摩子電影":
+        content = "很抱歉本項服務尚未建置完成，敬請選擇其他服務"
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=content))
+        return 0
+    '''
     if event.message.text == "eyny":
         content = eyny_movie()
         line_bot_api.reply_message(
@@ -361,6 +454,7 @@ def handle_message(event):
             event.reply_token,
             TextSendMessage(text=content))
         return 0
+    '''
     if event.message.text == "開始玩":
         buttons_template = TemplateSendMessage(
             alt_text='開始玩 template',
@@ -390,6 +484,7 @@ def handle_message(event):
         )
         line_bot_api.reply_message(event.reply_token, buttons_template)
         return 0
+    '''
     if event.message.text == "新聞":
         buttons_template = TemplateSendMessage(
             alt_text='新聞 template',
@@ -482,7 +577,7 @@ def handle_message(event):
         )
         line_bot_api.reply_message(event.reply_token, buttons_template)
         return 0
-
+'''
     buttons_template = TemplateSendMessage(
         alt_text='目錄 template',
         template=ButtonsTemplate(
